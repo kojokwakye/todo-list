053e11946cec22395d78838ba4b66fe4a8d15d96
kojokwakye <kojokwakye808@gmail.com>
2025-11-07 18:19:59 +0000

    remove svgs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import "./style.css";
 
-class list {
-  constructor() {
-    if (!new.target) throw new Error("Must use 'new' with Book");
-  }
-}
+// class list {
+//   constructor() {
+//     if (!new.target) throw new Error("Must use 'new' with Book");
+//   }
+// }
+
+

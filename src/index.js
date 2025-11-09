602827a762fa3aafcde9f1001036fc9d5b66a847
kojokwakye <kojokwakye808@gmail.com>
2025-11-09 23:38:49 +0000

    restart program

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import "./style.css";
 
-// class list {
-//   constructor() {
-//     if (!new.target) throw new Error("Must use 'new' with Book");
-//   }
-// }
+class List {
+  constructor() {
+    if (!new.target) throw new Error("Must use 'new' with List");
+  }
+}
 
 
